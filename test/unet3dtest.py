2be6fda7814a303ab48d34441d2c2fb17212d8b4
junqiangchen <1207173174@qq.com>
2018-03-19 12:40:18 +0800

    Update unet3dtest.py

--- test/unet3dtest.py
+++ test/unet3dtest.py
@@ -2,6 +2,7 @@ from __future__ import division
 from unet3d.model import unet3dModule
 import numpy as np
 import pandas as pd
+import cv2
 
 
 def train():
@@ -9,42 +10,40 @@ def train():
    Preprocessing for dataset
    '''
     # Read  data set (Train data from CSV file)
-    csvmaskdata = pd.read_csv('mask.csv')
-    csvimagedata = pd.read_csv('image.csv')
+    csvmaskdata = pd.read_csv('D:\Project\python\\3D_luna16-lung-cancer\data/mask3d_lidc.csv')
+    csvimagedata = pd.read_csv('D:\Project\python\\3D_luna16-lung-cancer\data/image3d_lidc.csv')
     maskdata = csvmaskdata.iloc[:, :].values
     imagedata = csvimagedata.iloc[:, :].values
     # shuffle imagedata and maskdata together
     perm = np.arange(len(csvimagedata))
     np.random.shuffle(perm)
-    imagedata = imagedata[perm]
-    maskdata = maskdata[perm]
-    # Extracting images and labels from given data
-    imagedata = imagedata.astype(np.float)
-    maskdata = maskdata.astype(np.float)
-    # Normalize from [0:255] => [0.0:1.0]
-    images = np.multiply(imagedata, 1.0 / 255.0)
-    labels = np.multiply(maskdata, 1.0 / 255.0)
-    # Split data into training & validation
-    train_images = images[0:]
-    train_labels = labels[0:]
+    train_images = imagedata[perm]
+    train_labels = maskdata[perm]
 
     unet3d = unet3dModule(3, 512, 512, 1)
-    unet3d.train(train_images, train_labels, "E:\pythonworkspace\\test\\model\\unet3d",
-                 "E:\\pythonworkspace\\test\\log", 0.0001, 0.8, 0.7, 50000, 1)
+    unet3d.train(train_images, train_labels, "D:\Project\python\download_projects\\Unet_3d-master\\test\\unet3d",
+                 "D:\Project\python\download_projects\\Unet_3d-master\\test\\log", learning_rate=0.0001,
+                 dropout_conv=0.8, train_epochs=5,
+                 batch_size=1)
 
 
 def predict():
-    test_imagesdata = pd.read_csv("test.csv")
-    # For images
-    image_size = 512 * 512 * 128
-    test_images = test_imagesdata[:, image_size:]
-    test_images = test_images.astype(np.float)
+    image1 = cv2.imread("D:\Project\python\download_projects\\Unet_3d-master\\1.bmp", cv2.IMREAD_GRAYSCALE)
+    image2 = cv2.imread("D:\Project\python\download_projects\\Unet_3d-master\\2.bmp", cv2.IMREAD_GRAYSCALE)
+    image3 = cv2.imread("D:\Project\python\download_projects\\Unet_3d-master\\3.bmp", cv2.IMREAD_GRAYSCALE)
+    testimage = np.empty((3, 512, 512, 1))
+    testimage[0, ...] = np.reshape(image1, (512, 512, 1))
+    testimage[1, ...] = np.reshape(image2, (512, 512, 1))
+    testimage[2, ...] = np.reshape(image3, (512, 512, 1))
+    test_images = testimage.astype(np.float)
     # convert from [0:255] => [0.0:1.0]
     test_images = np.multiply(test_images, 1.0 / 255.0)
-    unet3d = unet3dModule(128, 512, 512, 1)
-    predictvalue = unet3d.prediction("E:\pythonworkspace\\test\\model\\unet3d",
+    unet3d = unet3dModule(3, 512, 512, 1)
+    predictvalue = unet3d.prediction("D:\Project\python\download_projects\\Unet_3d-master\\test\\unet3d",
                                      test_images)
-    print(predictvalue[0])
+    cv2.imwrite("mask1.bmp", predictvalue[0])
+    cv2.imwrite("mask2.bmp", predictvalue[1])
+    cv2.imwrite("mask3.bmp", predictvalue[2])
 
 
 def main(argv):
